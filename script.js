bb9ff01ffc2df20155caef769d05b30346293ac3
EDLR <160347985+ErikDlrr@users.noreply.github.com>
2024-03-28 16:14:49 -0600

    Add files via upload

--- script.js
+++ script.js
@@ -2,3 +2,27 @@ function toggleNavbar() {
     var navbar = document.querySelector('.navbar');
     navbar.classList.toggle('expanded');
 }
+
+document.addEventListener('DOMContentLoaded', function () {
+    const modal = document.querySelector('.modal');
+    const modalImg = document.getElementById('expanded-img');
+    const galleryImages = document.querySelectorAll('.gallery-image');
+    const closeBtn = document.querySelector('.close');
+
+    galleryImages.forEach(function (img) {
+        img.addEventListener('click', function () {
+            modal.style.display = 'block';
+            modalImg.src = this.src;
+        });
+    });
+
+    closeBtn.addEventListener('click', function () {
+        modal.style.display = 'none';
+    });
+
+    window.addEventListener('click', function (e) {
+        if (e.target == modal) {
+            modal.style.display = 'none';
+        }
+    });
+});
